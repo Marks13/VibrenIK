8f86a8bdb3db244314c780f79766787ca86b8007
Stéphane Zucatti <stephane.zucatti@cerema.fr>
2016-08-29 18:18:20 +0200

    Update# 107

--- src/Contents/Application/app/controller/CMain.js
+++ src/Contents/Application/app/controller/CMain.js
@@ -118,7 +118,7 @@ App.controller.define('CMain', {
                 title: 'Voie ' + index,
                 listeners: {
                     single: true,
-                    render: function (tab, e0pts) {
+                    tabchange: function (tab, e0pts) {
                         console.log("listener");
                         _p.plot(tab.id, index);
                     }
