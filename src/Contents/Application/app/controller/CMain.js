f5225b14228c8b13e591af045a048f5286aa69f2
Stéphane Zucatti <stephane.zucatti@cerema.fr>
2016-08-29 18:22:16 +0200

    Update# 110

--- src/Contents/Application/app/controller/CMain.js
+++ src/Contents/Application/app/controller/CMain.js
@@ -118,7 +118,7 @@ App.controller.define('CMain', {
                 title: 'Voie ' + index,
                 listeners: {
                     single: true,
-                    blur: function (tab, e0pts) {
+                    blur: function (tab, e, e0pts) {
                         console.log("listener");
                         _p.plot(tab.id, index);
                     }
