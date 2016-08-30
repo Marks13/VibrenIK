0ee118202376e165895c86d16bf3fed005eb19e2
Stéphane Zucatti <stephane.zucatti@cerema.fr>
2016-08-30 11:22:52 +0200

    Update# 175

--- src/Contents/Services/PdfService.js
+++ src/Contents/Services/PdfService.js
@@ -1,14 +1,17 @@
 PdfService = {
-    renderPdf: function (o, cb) {
+    renderPdf: function (mesure, res, cb) {
         PDFDocument = require('pdfkit');
         blobStream = require('blob-stream');
 
         doc = new PDFDocument;
 
-        stream = doc.pipe(res);
+        stream = doc.pipe(blobStream());
         doc.fontSize(25).text('Here is some vector graphics...', 100, 80);
         doc.end();
-
+        stream.on('finish', function () {
+            blob = stream.toBlob('application/pdf');
+            res.send(blob);
+        });
     }
 
 };
