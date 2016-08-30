3278883086d7b591d298ef4581f55bbc3285693f
Stéphane Zucatti <stephane.zucatti@cerema.fr>
2016-08-30 10:25:05 +0200

    Update# 158

--- src/System/app.js
+++ src/System/app.js
@@ -1,9 +1,15 @@
 App = {
-	init: function(app,server) {
-		app.post('/',app.UPLOAD.any(),function(req,res,next) {
-			App.upload.up(req,res);
-		});
-	}
+    init: function (app, server) {
+        app.post('/', app.UPLOAD.any(), function (req, res, next) {
+            App.upload.up(req, res);
+        });
+    },
+
+    pdf: function (id) {
+        app.post('/pdf.pdf', function (req, res) {
+            res.send('Got a POST request');
+        });
+    }
 };
 
 module.exports = App;
