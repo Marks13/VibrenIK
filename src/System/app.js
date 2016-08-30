06f1b0fc795b9a58f7c186acc6ebb6bd08c56cf0
Stéphane Zucatti <stephane.zucatti@cerema.fr>
2016-08-30 11:05:55 +0200

    Update# 165

--- src/System/app.js
+++ src/System/app.js
@@ -3,6 +3,15 @@ App = {
         app.post('/', app.UPLOAD.any(), function (req, res, next) {
             App.upload.up(req, res);
         });
+
+
+        app.get('/export/:id', function (req, res) {
+            if (isNaN(parseInt(req.param('id')))) {
+                res.respond(new Error('ID must be a valid integer'), 400);
+            } else {
+                res.send('Got a POST request');
+            }
+        });
     }
 };
 
