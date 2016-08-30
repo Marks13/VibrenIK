d9f92efe2cd47f268cc8701a6834a6fac5e281a0
Stéphane Zucatti <stephane.zucatti@cerema.fr>
2016-08-30 09:55:24 +0200

    Update# 150

---
+++ src/Contents/Services/PdfService.js
@@ -0,0 +1,15 @@
+PdfService = {
+    getByEtude: function (o, cb) {
+        var vibren = Etudes_listes.using('db').using('vibren');
+
+        vibren.etudes_listes.findAll({
+            where: {
+                etudeId: o
+            }
+        }).then(function (records) {
+            cb(records);
+        });
+    }
+};
+
+module.exports = PdfService;
