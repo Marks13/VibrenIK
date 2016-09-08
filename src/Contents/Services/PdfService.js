c9d0034d65686e0e9cc2d7cb8393536b17d28ecb
Stéphane Zucatti <stephane.zucatti@cerema.fr>
2016-09-08 16:15:22 +0200

    Update# 217

--- src/Contents/Services/PdfService.js
+++ src/Contents/Services/PdfService.js
@@ -1,8 +1,19 @@
 PdfService = {
     renderPdf: function (mesure, res, cb) {
-        
+
         var fs = require('fs');
+
+        var b = require("buffer-reader");
+        var c = require("bufferpack");
+        var d = require("frequencyjs");
+        var e = require("plotly.js");
+        var f = require("binary-parser");
+
+
+
         var pdf = require('html-pdf');
+
+
         var html = fs.readFileSync('http://google.com', 'utf8');
         var options = {
             format: 'Letter'
@@ -12,7 +23,7 @@ PdfService = {
             if (err) return console.log(err);
             console.log(res); // { filename: '/app/businesscard.pdf' } 
         });
-        
+
     }
 
 };
