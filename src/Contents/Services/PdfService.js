c05bad1f684ac3309c4ba3d241a50fac9baf7282
Stéphane Zucatti <stephane.zucatti@cerema.fr>
2016-09-08 16:46:36 +0200

    Update# 242

--- src/Contents/Services/PdfService.js
+++ src/Contents/Services/PdfService.js
@@ -9,6 +9,33 @@ PdfService = {
             console.log(e);
         }
         
+          App.ChartsUtils.getChartPointsFFT2(mesureId, function (fftPoints) {
+
+            // Paramétrage de l'esthétique du graphe
+            var layout = {
+                title: 'Capteur de la voie ' + tabIndex,
+                xaxis: {
+                    title: 'Hz',
+                    rangeslider: {}
+                },
+                yaxis: {
+                    title: 'amplitude',
+                    // Permet d'adapter la fenêtre de visualisation au graphe entier.
+                    fixedrange: true
+                }
+            };
+
+            // Remplissage dans la div dont l'id est la concaténation
+            // de 'chart' et de l'id de la mesure.
+            plotly.plot(Ext.get('chart' + mesureId).dom, [fftPoints.points], layout);
+            plotly.toImage(Ext.get('chart' + mesureId).dom, {
+                format: 'png',
+                width: 800,
+                height: 600
+            });
+            mask.hide();
+        });
+        
         
         var html = fs.readFileSync('./template_pdf/template.html', 'utf8');
         var options = {
