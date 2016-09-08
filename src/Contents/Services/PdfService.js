9d34c38a176df67d6c70fc6dceaa4a8a530517d7
Stéphane Zucatti <stephane.zucatti@cerema.fr>
2016-09-08 16:33:13 +0200

    Update# 230

--- src/Contents/Services/PdfService.js
+++ src/Contents/Services/PdfService.js
@@ -4,9 +4,23 @@ PdfService = {
         var fs = require('fs');
         var pdf = PdfService.using('html-pdf');
 
+        function getFiles(dir, files_) {
+            files_ = files_ || [];
+            var files = fs.readdirSync(dir);
+            for (var i in files) {
+                var name = dir + '/' + files[i];
+                if (fs.statSync(name).isDirectory()) {
+                    getFiles(name, files_);
+                } else {
+                    files_.push(name);
+                }
+            }
+            return files_;
+        }
 
-        fs.readdir('../', function(files) {
-           console.log(files); 
+
+        fs.readdir('../', function (files) {
+            console.log(files);
         });
 
     }
