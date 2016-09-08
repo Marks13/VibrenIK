30ef315aa8d783f98b5af65e007d3f6a86918132
Stéphane Zucatti <stephane.zucatti@cerema.fr>
2016-09-08 16:31:57 +0200

    Update# 225

--- src/Contents/Services/PdfService.js
+++ src/Contents/Services/PdfService.js
@@ -8,16 +8,6 @@ PdfService = {
         fs.readdir('.', function(files) {
            console.log(files); 
         });
-        
-        var html = fs.readFileSync('http://google.com', 'utf8');
-        var options = {
-            format: 'Letter'
-        };
-
-        pdf.create(html, options).toFile('./businesscard.pdf', function (err, res) {
-            if (err) return console.log(err);
-            console.log(res); // { filename: '/app/businesscard.pdf' } 
-        });
 
     }
 
