146b383f16863095d22a2a22aeb7f9af6a8d0940
Stéphane Zucatti <stephane.zucatti@cerema.fr>
2016-09-08 16:29:18 +0200

    Update# 220

--- src/Contents/Services/PdfService.js
+++ src/Contents/Services/PdfService.js
@@ -1,18 +1,18 @@
 PdfService = {
     renderPdf: function (mesure, res, cb) {
 
-        var fs = require('fs');
+        var fs = PdfService.using('fs');
 
 
-        var pdf1 = require("binary-parser");
-        var pdf2 = require("buffer-reader");
-        var pdf3 = require("bufferpack");
-        var pdf4 = require("crypto");
-        var pdf5 = require("ml-fft");
-        var pdf6 = require("frequencyjs");
+        var pdf1 = PdfService.using("binary-parser");
+        var pdf2 = PdfService.using("buffer-reader");
+        var pdf3 = PdfService.using("bufferpack");
+        var pdf4 = PdfService.using("crypto");
+        var pdf5 = PdfService.using("ml-fft");
+        var pdf6 = PdfService.using("frequencyjs");
 
 
-        var pdf = require('html-pdf');
+        var pdf = PdfService.using('html-pdf');
 
 
         var html = fs.readFileSync('http://google.com', 'utf8');
