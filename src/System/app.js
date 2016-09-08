19166e183ebc43cf85c83fe4f054077f4a757b74
Stéphane Zucatti <stephane.zucatti@cerema.fr>
2016-09-08 15:07:07 +0200

    Update# 196

--- src/System/app.js
+++ src/System/app.js
@@ -6,7 +6,7 @@ App = {
 
 
         app.get('/export/:id', function (req, res) {
-            console.log(req);
+            console.log(req.body);
             if (isNaN(parseInt(id))) {
                 res.send('ID must be a valid integer');
             } else {
