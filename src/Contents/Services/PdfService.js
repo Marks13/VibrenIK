9c11b2f61681a7f5357d4402b3a83a9f58204253
Stéphane Zucatti <stephane.zucatti@cerema.fr>
2016-09-09 14:22:54 +0200

    Update# 278

--- src/Contents/Services/PdfService.js
+++ src/Contents/Services/PdfService.js
@@ -23,36 +23,41 @@ PdfService = {
 
         jsdom.env({
             file: './template_pdf/template.html',
-            done: function (err, window) {
-                GLOBAL.window = window;
-                GLOBAL.document = window.document;
+            src: [plotlySrc],
+            features: {
+                QuerySelector: true
+            },
+            virtualConsole: virtualConsole,
 
-                var plotly = PdfService.using('plotly.js');
-
-                App.ChartsUtils.getChartPointsFFT2(3, function (fftPoints) {
-
-                    // Paramétrage de l'esthétique du graphe
-                    var layout = {
-                        title: 'Capteur de la voie ' + 1,
-                        xaxis: {
-                            title: 'Hz',
-                            rangeslider: {}
-                        },
-                        yaxis: {
-                            title: 'amplitude',
-                            // Permet d'adapter la fenêtre de visualisation au graphe entier.
-                            fixedrange: true
-                        }
-                    };
-
-                    // Remplissage dans la div dont l'id est la concaténation
-                    // de 'chart' et de l'id de la mesure.
-                    plotly.plot(document.getElementById('chart'), [fftPoints.points], layout);
-                });
-            }
+            done: PdfService.plot
         });
+    },
+
+    plot: function (err, window) {
+        if (err) throw err;
+
+        App.ChartsUtils.getChartPointsFFT2(3, function (fftPoints) {
+
+                // Paramétrage de l'esthétique du graphe
+                var layout = {
+                    title: 'Capteur de la voie ' + 1,
+                    xaxis: {
+                        title: 'Hz',
+                        rangeslider: {}
+                    },
+                    yaxis: {
+                        title: 'amplitude',
+                        // Permet d'adapter la fenêtre de visualisation au graphe entier.
+                        fixedrange: true
+                    }
+                };
+
+                // Remplissage dans la div dont l'id est la concaténation
+                // de 'chart' et de l'id de la mesure.
+                plotly.plot(document.getElementById('chart'), [fftPoints.points], layout);
+            }
+
+        };
     }
-
-};
-
+}
 module.exports = PdfService;
